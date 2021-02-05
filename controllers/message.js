f942c0ca86ef928ddde30ef732f2d5517b9f9092
Stasys Meclazcke <aeip@live.com>
2021-02-04 20:45:02 -0600

    More minor changes to structure

--- controllers/message.js
+++ controllers/message.js
@@ -3,23 +3,13 @@ const { Router } = require("express");
 const router = Router();
 
 //index route
-router.get("/", async (req, res) => {
+router.get("/:symbol/", async (req, res) => {
   res.json(await Message.find({}));
 });
 
 //create route
-router.post("/", async (req, res) => {
+router.post("/:symbol/", async (req, res) => {
   res.json(await Message.create(req.body));
-});
-
-//update route
-router.put("/:id", async (req, res) => {
-  res.json(await Message.findByIdAndUpdate(req.params.id, req.body, { new: true }));
-});
-
-//delete route
-router.delete("/:id", async (req, res) => {
-  res.json(await Message.findByIdAndRemove(req.params.id));
 });
 
 // EXPORT ROUTER
--- models/message.js
+++ models/message.js
@@ -4,7 +4,7 @@ const { Schema, model } = require("mongoose");
 const messageSchema = new Schema(
   {
     username: String,
-    message: Number,
+    message: String,
   },
   { timestamps: true }
 );
